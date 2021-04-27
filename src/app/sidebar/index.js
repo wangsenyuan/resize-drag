3b2ffcedcb28d0358fb481c07845c7677272785a
WangSenyuan <brucewang@maycur.com>
2021-04-27 10:12:03 +0800

    add sidebar state

--- src/app/sidebar/index.js
+++ src/app/sidebar/index.js
@@ -1,6 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+
+// this is a sample
+const printObjectState = {
+  name: "reimburseForm",
+  label: "报销单",
+  fields: [
+    { name: "formCode", label: "单据号" },
+    { name: "comment", label: "备注" },
+  ],
+  children: [
+    {
+      name: "feeList",
+      label: "费用明细",
+      relationType: "MANY",
+      fields: [
+        { name: "amount", label: "金额" },
+        { name: "comment", label: "事由" },
+      ],
+    },
+    {
+      name: "department",
+      label: "报销部门",
+      fields: [
+        { name: "name", label: "部门名称" },
+        { name: "businessCode", label: "部门编码" },
+      ],
+    },
+  ],
+};
 
 function Page({ className }) {
+  const [state] = useState(printObjectState);
   return <div className={`${className ?? ""}`}></div>;
 }
 
